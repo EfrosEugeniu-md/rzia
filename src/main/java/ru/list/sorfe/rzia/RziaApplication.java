55424268062780551e131ee00b4fcaf4d9566f0d
EfrosEugeniu-md <64984462+EfrosEugeniu-md@users.noreply.github.com>
2021-04-15 10:30:05 +0300

    first commit

---
+++ src/main/java/ru/list/sorfe/rzia/RziaApplication.java
@@ -0,0 +1,13 @@
+package ru.list.sorfe.rzia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RziaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RziaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/list/sorfe/rzia/RziaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.list.sorfe.rzia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RziaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
